46bb6a57570712baa4634e0c873fe74f83683182
NancyNi27 <wni431@aucklanduni.ac.nz>
2026-01-30 15:54:18 +1300

    pbi finished

--- md/bank-loan-domain knowledge.md
+++ md/bank-loan-domain knowledge.md
@@ -31,4 +31,11 @@ Bank loan is a financial tool that enables individuals and business to achieve t
 ## Why should we analyse Bank Loan Data
 1. **Risk Assessment:** One of the primary purposes of analysing loan data is to assess the risk associated with lending to a particular individual or business. Banks use data to evaluate the creditworthiness of borrowers, predict default probabilities, and determine interest rates and lending terms.
 2. **Decision-making:** Loan data analysis supports the decision-making process when evaluating loan applications. Banks use data-driven models and algorithms to make informed lending decisions, such as approving or denying loan requests.
-3. **Portfolio Management:** Banks manage portfolios of loans, including mortages(抵押)
+3. **Portfolio Management:** Banks manage portfolios of loans, including mortages(抵押), personal loans, and business loans. Data analysis helps banks monitor the health of these portfolios, identify underperforming loans, and optimize loan terms and pricing.
+4. **Fraud Detection:** Banks use data analysis to detetct fraudulent loan applications and activities. Unusual patterns, inconsistencies, or discrepanies in loan data can trigger fraud alerts.
+5. **Regulatory Compliance:** Banks are subject to regulatory requirements that mandate the collection and reporting of loan data. Compliance with regulations such as the Home Mortgage Disclosure Act(HMDA) and the Know Your Cutsomer(KYC) regulations requires data analysis and reporting.
+6. **Customer Insight:** Analysing loan data provides insights into customer behaviour , preferences, and needs. Banks can use these insights to tailor loan products and marketing strategies to specific customer segments.
+7. **Profitability Analysis**: Banks assess the profitability of their loan portfolios by analysing data related to interest income, loan origination costs, default rates, and collection efforts.
+8. **Market Research:** Data analysis helps banks understand market trends, competitive landscape, and customer demand. This information guides product development and market expansion strategies.
+9. **Credit Risk Management:** Banks continuously monitor and manage credit risk associated with their loans. Data analysis helps in setting risk management strategies, provisioning for potential losses, and stress testing loan portfolios.
+10.**Customer Retention(保留):** Banks use data analysis to identify opportunities for retaining existing customers, such as offering loan refinancing options or additional financial products.
--- sql/SQLQuery1.sql
+++ sql/SQLQuery1.sql
@@ -29,3 +29,145 @@ WHERE MONTH(issue_date) = 12;
 --PMTD
 SELECT SUM(total_payment) AS Total_Amount_CollectedMTD FROM financial_loan
 WHERE MONTH(issue_date) = 11;
+
+--Interest Rate
+--Average Interest Rate
+SELECT AVG(int_rate)*100 AS Avg_Int_Rate FROM financial_loan;
+--MTD Average Interest Rate
+SELECT AVG(int_rate)*100 AS Avg_Int_Rate FROM financial_loan
+WHERE MONTH(issue_date)=12;
+--PMTD
+SELECT AVG(int_rate)*100 AS Avg_Int_Rate FROM financial_loan
+WHERE MONTH(issue_date)=11;
+
+--DTI(debt to income ratios)
+--Avg DTI
+SELECT AVG(dti)*100 AS Avg_DTI FROM financial_loan;
+--MTD
+SELECT AVG(dti)*100 AS MTD_AVG_DTI FROM financial_loan
+WHERE MONTH(issue_date) = 12;
+--PMTD
+SELECT AVG(dti)*100 AS MTD_AVG_DTI FROM financial_loan
+WHERE MONTH(issue_date) = 11;
+
+--Good loan issued
+--Percentage of Good Loan
+SELECT (COUNT(CASE WHEN loan_status = 'Fully Paid' OR loan_status = 'Current' THEN id END) * 100.0)/COUNT(id) AS Good_Loan_Percentage
+FROM financial_loan;
+--Good Loan Applications
+SELECT COUNT(id) AS Good_Loan_Applications FROM financial_loan
+WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
+--Good Loan Funded Amount
+SELECT SUM(loan_amount) AS Good_Loan_Funded_amount
+FROM financial_loan
+WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
+--Good Loan Amount Received
+SELECT SUM(total_payment) AS Good_Loan_amount_received 
+FROM financial_loan
+WHERE loan_status = 'Fully Paid' OR loan_status = 'Current';
+
+--Bad Loan Issued
+--Bad Loan Percentage
+SELECT (COUNT(CASE WHEN loan_status = 'Charged Off' THEN id END)*100.0)/COUNT(id) AS Bad_Loan_Percentage
+FROM financial_loan
+--Bad Loan Applications
+SELECT COUNT(id) AS Bad_Loan_Applications FROM financial_loan
+WHERE loan_status = 'Charged Off'
+--Bad Loan Funded Amount
+SELECT SUM(loan_amount) AS Bad_loan_funded_amount
+FROM financial_loan
+WHERE loan_status = 'Charged Off'
+--Bad Loan Amount Received
+SELECT SUM(total_payment) AS Bad_Loan_amount_received 
+from financial_loan
+WHERE loan_status = 'Charged Off'
+
+--Loan Status
+SELECT loan_status,
+	COUNT(id) AS Total_Amount_Received,
+	SUM(total_payment) AS Total_Amount_Received,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	AVG(int_rate *100) AS Interest_Rate,
+	AVG(dti*100) AS DTI
+FROM financial_loan
+GROUP BY loan_status;
+--MTD
+SELECT loan_status,
+	COUNT(id) AS Total_Amount_Received,
+	SUM(total_payment) AS Total_Amount_Received,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	AVG(int_rate *100) AS Interest_Rate,
+	AVG(dti*100) AS DTI
+FROM financial_loan
+WHERE MONTH(issue_date) = 12
+GROUP BY loan_status;
+
+--OverView
+--Monthly
+SELECT 
+	MONTH(issue_date) AS Month_Number,
+	DATENAME(MONTH, issue_date) AS Month_name,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_funded_amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM financial_loan
+GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
+ORDER BY MONTH(issue_date);
+
+--State
+SELECT 
+	address_state AS State,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM financial_loan
+GROUP BY address_state
+ORDER BY address_state;
+
+--TERM
+SELECT 
+	term AS Term,
+	COUNT(id) AS Total_Loan_Applcations,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM financial_loan
+GROUP BY term
+ORDER BY term
+
+--EMPLOYEE LENGTH
+SELECT
+	emp_length AS Employee_Length,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM financial_loan
+GROUP BY emp_length
+ORDER BY emp_length;
+
+--Purpose
+SELECT purpose AS PURPOSE,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM financial_loan
+GROUP BY purpose
+ORDER BY purpose;
+
+--Home Ownership
+SELECT home_ownership AS Home_Ownership,
+	COUNT(id) AS Total_Loan_applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received 
+FROM financial_loan
+GROUP BY home_ownership
+ORDER BY home_ownership;
+
+
+
+
+
+
+
+
+
+
